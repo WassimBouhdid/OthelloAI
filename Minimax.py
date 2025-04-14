7c5ff07a13d784682866f6c3e89c4cc760cb6da0
Bouhdid Wassim <64103126+WassimBouhdid@users.noreply.github.com>
2025-04-15 01:17:27 +0200

    new way of computing best moves in minimax

--- Minimax.py
+++ Minimax.py
@@ -9,46 +9,68 @@ class MiniMax:
     def __init__(self):
         self.count = 0
 
-    def minimax(self, board, depth, is_maximizing):
+    def minimax(self, alpha, beta, board, depth, is_maximizing):
+
         # print(depth)
 
         # print(self.count)
         # self.count +=1
-        if board.is_game_finished(is_maximizing):
-            if board.compute_winner():
-                return DIMENSION * DIMENSION + depth
-            else:
-                return DIMENSION * DIMENSION - depth
 
         board.compute_possible_moves(is_maximizing)
+
+        if depth == 0:
+            return self.board_evaluation(board, is_maximizing)
+        elif board.is_game_finished(is_maximizing):
+            if board.compute_winner():
+                return math.inf
+            else:
+                return -math.inf
+
         best_score = -math.inf if is_maximizing else math.inf
-        # print(board.get_possible_moves())
+        best_move = []
+
         if board.get_possible_moves():
             for i in board.get_possible_moves():
+
                 copy_board = copy.deepcopy(board)
-                board.set_pawns(is_maximizing, i[0], i[1])
-                score = self.minimax(board, depth + 1, 1 - is_maximizing)
-                board.set_board(copy_board.get_board())
+                copy_board.set_pawns(is_maximizing, i[0], i[1])
+                score = self.minimax(board, alpha, beta, depth - 1, 1 - is_maximizing)
                 best_score = max(best_score, score) if is_maximizing else min(best_score, score)
+
+                if is_maximizing:
+                    if score > best_score:
+                        best_score = score
+                        best_move = i
+                    alpha = max(alpha, best_score)
+                else:
+                    if score < best_score:
+                        best_score = score
+                        best_move = i
+                    beta = min(beta, best_score)
+
+                if alpha >= beta:
+                    break
+                # max_score = score
+                # move = (i[0], i[1])
         else:
-            # print("pas de move pour", is_maximizing)
-            best_score = self.minimax(board, depth + 1, 1 - is_maximizing)
+            best_score = self.minimax(board, alpha, beta, depth - 1, 1 - is_maximizing)
 
-        return best_score
+        return best_move
 
-    def best_move(self, board, player):
-        board.compute_possible_moves(player)
-        max_score = -math.inf
-        move = None
-        print(board.get_possible_moves())
-        for i in board.get_possible_moves():
-            copy_board = copy.deepcopy(board)
-            board.set_pawns(1, i[0], i[1])
-            score = self.minimax(board, 0, 1 - player)
-            board.set_board(copy_board.get_board())
-            print(score)
-            if score > max_score:
-                max_score = score
-                move = (i[0], i[1])
-        # print(self.count)
-        return move
+    # def best_move(self, board, player):
+    #     board.compute_possible_moves(player)
+    #     max_score = -math.inf
+    #     move = None
+    #     for i in board.get_possible_moves():
+    #         copy_board = copy.deepcopy(board)
+    #         copy_board.set_pawns(1, i[0], i[1])
+    #         score = self.minimax(board, 0, 1 - player)
+    #         print(score)
+    #         if score > max_score:
+    #             max_score = score
+    #             move = (i[0], i[1])
+    #     # print(self.count)
+    #     return move
+
+    def board_evaluation(self, board, is_maximizing):
+        pass
